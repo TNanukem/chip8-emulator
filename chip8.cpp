367d0a030bc6727e75f6e37be6daf93852db6d39
Tiago Toledo Junior <tiago.nanu@gmail.com>
2018-10-20 19:40:29 -0300

    More opcodes added, also added some documentation to each opcode functionality

--- chip8.cpp
+++ chip8.cpp
@@ -22,10 +22,12 @@ void chip8::nextCycle(){
 					break;
 			}
 
+		// Jumps to the address NNN
 		case 0x1000:
 			this->pc = (opcode & GET_ADDRESS);
 			break;
 
+		// Skips the next instruction if v[X] is equal to NN
 		case 0x3000:
 			if(v[opcode & GET_X] == (opcode & GET_8BIT_CONSTANT)){
 					this->pc += 4;
@@ -33,6 +35,7 @@ void chip8::nextCycle(){
 			else this->pc += 2;
 			break;
 
+		// Skips the next instruction if v[X] is not equal to NN
 		case 0x4000:
 			if(v[opcode & GET_X] != (opcode & GET_8BIT_CONSTANT)){
 					this->pc += 4;
@@ -40,6 +43,7 @@ void chip8::nextCycle(){
 			else this->pc += 2;
 			break;
 
+		// Skips the next instruction if v[X] is equal to v[Y]
 		case 0x5000:
 			if(v[opcode & GET_X] == v[opcode & GET_Y]){
 				this->pc += 4;
@@ -47,16 +51,19 @@ void chip8::nextCycle(){
 			else this->pc += 2;
 			break;
 
+		// Sets v[X] to NN
 		case 0x6000:
 			v[opcode & GET_X] = (opcode & GET_8BIT_CONSTANT);
 			this->pc += 2;
 			break;
 
+		// Adds NN to v[X]
 		case 0x7000:
 			v[opcode & GET_X] += (opcode & GET_8BIT_CONSTANT);
 			this->pc += 2;
 			break;
 
+		// Skips the next instruction if v[X] is not equal to v[Y]
 		case 0x9000:
 			if(v[opcode & GET_X] != v[opcode & GET_Y]){
 				this->pc += 4;
@@ -64,21 +71,69 @@ void chip8::nextCycle(){
 			else this->pc += 2;
 			break;
 
+		case 0x8000:
+			switch(opcode & GET_4BIT_CONSTANT){
+
+				// Sets v[X] to v[Y]
+				case 0x0000:
+					v[opcode & GET_X] = v[opcode & GET_Y];
+					break;
+
+				// Sets v[X] to v[X] OR v[Y]
+				case 0x0001:
+					v[opcode & GET_X] = v[opcode & GET_X] | v[opcode & GET_Y];
+					break;
+
+				// Sets v[X] to v[X] AND v[Y]
+				case 0x0002:
+					v[opcode & GET_X] = v[opcode & GET_X] & v[opcode & GET_Y];
+					break;
+
+				// Sets v[X] to v[X] XOR v[Y]
+				case 0x0003:
+
+					break;
+
+				case 0x0004:
+
+					break;
+
+				case 0x0005:
+
+					break;
+
+				case 0x0006:
+
+					break;
+
+				case 0x0007:
+
+					break;
+
+				case 0x000E:
+
+					break;
+			}
+
+		// Sets the I address to NNN
 		case 0xA000:
 			this->I = opcode & GET_ADDRESS;
 			this->pc += 2;
 			break;
 
+		// Jumps to the address V0 + NNN
 		case 0xB000:
 			this->pc = (opcode & GET_ADDRESS) + this->v[0];
 			this->pc += 2;
 			break;
 
+		// Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
 		case 0xC000:
 			//v[opcode & GET_X] = rand() & (opcode & GET_8BIT_CONSTANT);
 			this->pc += 2;
 			break;
 
+		// Draws a sprite at (vx,vy) with size height of N pixels
 		case 0xD000:
 			//draw(v[opcode & GET_X],v[opcode & GET_Y], (opcode & GET_4BIT_CONSTANT));
 			this->pc += 2;
