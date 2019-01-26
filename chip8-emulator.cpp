11cf88ce67cf11521299a18cee02b7b2adfe3bac
Tiago Toledo Junior <tiago.nanu@gmail.com>
2019-01-26 15:30:58 +0000

    Added the keyboard to the system. Now the user can pause the game and the structures to the load and save state are also done

--- chip8-emulator.cpp
+++ chip8-emulator.cpp
@@ -8,10 +8,11 @@ int main (void){
 	chip8 Chip8;
 	uint32_t buffer[2048];
 	uint8_t pixel;
+	uint8_t slot = 0;
 
 	// Intializes the Chip8 system and loads the game into memory
 	Chip8.initialize();
-	Chip8.loadGame("roms/KALEID");
+	Chip8.loadGame("roms/PONG");
 
 	// Graphical System Initialization
 	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -41,12 +42,87 @@ int main (void){
 
 		SDL_Event event;
 
-		// Event loop
+		// Event loop -> This is what controls the keyboard inputs
 		while(SDL_PollEvent(&event)){
 			if(event.type == SDL_QUIT)		// Closes the window when the user clicks on the close button
 				return 0;
+
+			// Structure that pauses the game when the user presses the spacebar
+			if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE){
+				printf("Game Paused\n");
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(800));
+				while(1){
+					SDL_PollEvent(&event);
+					if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE)
+						break;
+				}
+				printf("Unpausing Game\n");
+			}
+
+			// Selects the load/save state slot
+			if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_F1){
+				if(slot == 3)
+					slot = 0;
+				else
+					slot++;
+				printf("Selected slot %d\n", slot);
+			}
+
+			// Saves the game on the selected slot
+			if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_F2){
+				printf("Saving the game on slot %d\n", slot);
+				// Chip8.saveGame(slot)
+			}
+
+			// Loads the game from the selected slot
+			if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_F3){
+				printf("Loading the game from slot %d\n", slot);
+				// Chip8.loadGame(slot)
+			}
+
+			// All the pressed keys options
+			if(event.type == SDL_KEYUP){
+				if(event.key.keysym.sym == SDLK_1)	Chip8.keyboard[0] = 0;
+				if(event.key.keysym.sym == SDLK_2)	Chip8.keyboard[1] = 0;
+				if(event.key.keysym.sym == SDLK_3)	Chip8.keyboard[2] = 0;
+				if(event.key.keysym.sym == SDLK_4)	Chip8.keyboard[3] = 0;
+				if(event.key.keysym.sym == SDLK_q)	Chip8.keyboard[4] = 0;
+				if(event.key.keysym.sym == SDLK_w)	Chip8.keyboard[5] = 0;
+				if(event.key.keysym.sym == SDLK_e)	Chip8.keyboard[6] = 0;
+				if(event.key.keysym.sym == SDLK_r)	Chip8.keyboard[7] = 0;
+				if(event.key.keysym.sym == SDLK_a)	Chip8.keyboard[8] = 0;
+				if(event.key.keysym.sym == SDLK_s)	Chip8.keyboard[9] = 0;
+				if(event.key.keysym.sym == SDLK_d)	Chip8.keyboard[10] = 0;
+				if(event.key.keysym.sym == SDLK_f)	Chip8.keyboard[11] = 0;
+				if(event.key.keysym.sym == SDLK_z)	Chip8.keyboard[12] = 0;
+				if(event.key.keysym.sym == SDLK_x)	Chip8.keyboard[13] = 0;
+				if(event.key.keysym.sym == SDLK_c)	Chip8.keyboard[14] = 0;
+				if(event.key.keysym.sym == SDLK_v)	Chip8.keyboard[15] = 0;
+			}
+
+			// All the release keys options
+			if(event.type == SDL_KEYDOWN){
+				if(event.key.keysym.sym == SDLK_1)	Chip8.keyboard[0] = 1;
+				if(event.key.keysym.sym == SDLK_2)	Chip8.keyboard[1] = 1;
+				if(event.key.keysym.sym == SDLK_3)	Chip8.keyboard[2] = 1;
+				if(event.key.keysym.sym == SDLK_4)	Chip8.keyboard[3] = 1;
+				if(event.key.keysym.sym == SDLK_q)	Chip8.keyboard[4] = 1;
+				if(event.key.keysym.sym == SDLK_w)	Chip8.keyboard[5] = 1;
+				if(event.key.keysym.sym == SDLK_e)	Chip8.keyboard[6] = 1;
+				if(event.key.keysym.sym == SDLK_r)	Chip8.keyboard[7] = 1;
+				if(event.key.keysym.sym == SDLK_a)	Chip8.keyboard[8] = 1;
+				if(event.key.keysym.sym == SDLK_s)	Chip8.keyboard[9] = 1;
+				if(event.key.keysym.sym == SDLK_d)	Chip8.keyboard[10] = 1;
+				if(event.key.keysym.sym == SDLK_f)	Chip8.keyboard[11] = 1;
+				if(event.key.keysym.sym == SDLK_z)	Chip8.keyboard[12] = 1;
+				if(event.key.keysym.sym == SDLK_x)	Chip8.keyboard[13] = 1;
+				if(event.key.keysym.sym == SDLK_c)	Chip8.keyboard[14] = 1;
+				if(event.key.keysym.sym == SDLK_v)	Chip8.keyboard[15] = 1;
+			}
 		}
 
+		// Calls the next cycle, fetching, decoding and executing the next instruction in the memory
 		Chip8.nextCycle();
 
 		if(Chip8.drawFlag){
@@ -65,7 +141,7 @@ int main (void){
 			SDL_RenderPresent(render);
 		}
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(16));
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
 		SDL_RenderClear(render);
 	}
 
--- chip8.cpp
+++ chip8.cpp
@@ -3,6 +3,7 @@
 
 bool pressedKey = false;
 unsigned short pixel, xaux, yaux;
+int height;
 
 unsigned char chip8_fontset[80] = {
   0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -27,35 +28,35 @@ void chip8::initialize(){
 
 	// Clears the memory of the system
 	for(int i = 0; i < 4096; i++){
-		this->memory[i] = 0;
+		memory[i] = 0;
 	}
 
 	// Clears all registers and the stack
 	for(int i = 0; i < 16; i++){
-		this->v[i] = 0;
-		this->stack[i] = 0;
-        this->keyboard[i] = 0;
+		v[i] = 0;
+		stack[i] = 0;
+        keyboard[i] = 0;
 	}
 
 	// Clears the display
 	for(int i = 0; i < 2048; i++){
-		this->display[i] = 0;
+		display[i] = 0;
 	}
 
 	// Uploads the fontset to the Memory
 	for(int i = 0; i < 80; i++){
-		this->memory[i] = chip8_fontset[i];
+		memory[i] = chip8_fontset[i];
 	}
 
-	this->opcode = 0;
-	this->pc = 0x200;
-	this->sp = 0;
-	this->I = 0;
+	opcode = 0;
+	pc = 0x200;
+	sp = 0;
+	I = 0;
 
-	this->soundTimer = 0;
-	this->delayTimer = 0;
+	soundTimer = 0;
+	delayTimer = 0;
 
-	this->drawFlag = 1;
+	drawFlag = 1;
 
 }
 
@@ -81,19 +82,19 @@ bool chip8::loadGame(char *game){
 
 	// Writes the data on the memory
 	for(int i = 0; i < gameSize; i++){
-		this->memory[i + 512] = buffer[i];
+		memory[i + 512] = buffer[i];
         printf("Memory[%d] = %d\n", i+512,memory[i+512]);
 	}
 
 	fclose(gameP);
 	free(buffer);
-
+    return true;
 }
 
 void chip8::nextCycle(){
 
 	// Fetching the instruction
-	this->opcode = this->memory[this->pc] << 8 | this->memory[this->pc+1];
+	opcode = memory[pc] << 8 | memory[pc+1];
 
 	// Decoding the instruction
 	switch(opcode & GET_4BITS){					// Decode the instruction based on the first 4 bits
@@ -105,17 +106,17 @@ void chip8::nextCycle(){
 				// Clears the screen.
 				case 0x00E0:
 					for(int i = 0; i < 2048; i++){
-						this->display[i] = 0;
+						display[i] = 0;
 					}
-					this->pc += 2;
-					this->drawFlag = 1;
+					pc += 2;
+					drawFlag = 1;
                     printf("CLS\n");
 					break;
 
 				// Returns from a subroutine.
 				case 0x00EE:
-					this->sp--;
-					this->pc = this->stack[this->sp];
+					sp--;
+					pc = stack[sp];
                     printf("RET\n");
 					break;
 			}
@@ -123,14 +124,14 @@ void chip8::nextCycle(){
 
 		// Jumps to the address NNN
 		case 0x1000:
-			this->pc = (opcode & GET_ADDRESS);
+			pc = (opcode & GET_ADDRESS);
             printf("JMP %d\n", opcode & GET_ADDRESS);
 			break;
 
         // Calls subroutine at NNN.
 		case 0x2000:
-			this->stack[this->sp] = pc;
-			this->sp++;
+			stack[sp] = pc;
+			sp++;
 			pc = opcode & GET_ADDRESS;
             printf("CALL %d\n", opcode & GET_ADDRESS);
 			break;
@@ -138,28 +139,28 @@ void chip8::nextCycle(){
 		// Skips the next instruction if v[X] is equal to NN
 		case 0x3000:
 			if(v[(opcode & GET_X) >> 8] == (opcode & GET_8BIT_CONSTANT)){
-					this->pc += 4;
+					pc += 4;
 			}
-			else this->pc += 2;
+			else pc += 2;
             printf("SE V%d,%d\n", (opcode & GET_X) >> 8, opcode & GET_8BIT_CONSTANT);
 			break;
 
 		// Skips the next instruction if v[X] is not equal to NN
 		case 0x4000:
 			if(v[(opcode & GET_X) >> 8] != (opcode & GET_8BIT_CONSTANT)){
-					this->pc += 4;
+					pc += 4;
 			}
-			else this->pc += 2;
+			else pc += 2;
             printf("SNE V%d,%d\n", (opcode & GET_X) >> 8, opcode & GET_8BIT_CONSTANT);
 			break;
 
 		// Skips the next instruction if v[X] is equal to v[Y]
 		case 0x5000:
 			if(v[(opcode & GET_X) >> 8] == v[(opcode & GET_Y) >> 4]){
-				this->pc += 4;
+				pc += 4;
 			}
 			else
-				this->pc += 2;
+				pc += 2;
 
             printf("SE V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 			break;
@@ -167,23 +168,23 @@ void chip8::nextCycle(){
 		// Sets v[X] to NN
 		case 0x6000:
 			v[(opcode & GET_X) >> 8] = (opcode & GET_8BIT_CONSTANT);
-			this->pc += 2;
+			pc += 2;
             printf("LD V%d,%d\n", (opcode & GET_X) >> 8, opcode & GET_8BIT_CONSTANT);
 			break;
 
 		// Adds NN to v[X]
 		case 0x7000:
 			v[(opcode & GET_X) >> 8]  += (opcode & GET_8BIT_CONSTANT);
-			this->pc += 2;
+			pc += 2;
             printf("ADD V%d,%d\n", (opcode & GET_X) >> 8, opcode & GET_8BIT_CONSTANT);
 			break;
 
 		// Skips the next instruction if v[X] is not equal to v[Y]
 		case 0x9000:
 			if(v[opcode & GET_X >> 8] != v[(opcode & GET_Y) >> 4]){
-				this->pc += 4;
+				pc += 4;
 			}
-			else this->pc += 2;
+			else pc += 2;
             printf("SNE V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 			break;
 
@@ -193,28 +194,28 @@ void chip8::nextCycle(){
 				// Sets v[X] to v[Y]
 				case 0x0000:
 					v[(opcode & GET_X) >> 8] = v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("LD V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
 				// Sets v[X] to v[X] OR v[Y]
 				case 0x0001:
 					v[(opcode & GET_X) >> 8] |= v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("OR V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
 				// Sets v[X] to v[X] AND v[Y]
 				case 0x0002:
 					v[(opcode & GET_X) >> 8] &= v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("AND V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
 				// Sets v[X] to v[X] XOR v[Y]
 				case 0x0003:
 					v[(opcode & GET_X) >> 8] ^= v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("XOR V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
@@ -225,7 +226,7 @@ void chip8::nextCycle(){
 					else
 						v[0xF] = 0;
 					v[(opcode & GET_X) >> 8] += v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("ADD V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
@@ -236,15 +237,15 @@ void chip8::nextCycle(){
 					else
 						v[0xF] = 1;
 					v[(opcode & GET_X) >> 8] -= v[(opcode & GET_Y) >> 4];
-					this->pc += 2;
+					pc += 2;
                     printf("SUB V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
 				// Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
 				case 0x0006:
-					v[0xF] = v[(opcode & GET_X) >> 8] >> 7;
+					v[0xF] = v[(opcode & GET_X) >> 8] & 0x1;
 					v[(opcode & GET_X) >> 8] >> 1;
-					this->pc += 2;
+					pc += 2;
                     printf("SHR V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
@@ -255,7 +256,7 @@ void chip8::nextCycle(){
 					else
 						v[0xF] = 1;
 					v[(opcode & GET_X) >> 8] = v[(opcode & GET_Y) >> 4] - v[(opcode & GET_X) >> 8];
-					this->pc += 2;
+					pc += 2;
                     printf("SUBN V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 
@@ -263,7 +264,7 @@ void chip8::nextCycle(){
 				case 0x000E:
 					v[0xF] = v[(opcode & GET_X) >> 8] >> 7;
 					v[(opcode & GET_X) >> 8] << 1;
-					this->pc += 2;
+					pc += 2;
                     printf("SHL V%d,V%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4);
 					break;
 			}
@@ -271,22 +272,22 @@ void chip8::nextCycle(){
 
 		// Sets the I address to NNN
 		case 0xA000:
-			this->I = opcode & GET_ADDRESS;
-			this->pc += 2;
+			I = opcode & GET_ADDRESS;
+			pc += 2;
             printf("LD I, %d\n", opcode & GET_ADDRESS);
 			break;
 
 		// Jumps to the address V0 + NNN
 		case 0xB000:
-			this->pc = (opcode & GET_ADDRESS) + this->v[0];
-			this->pc += 2;
+			pc = (opcode & GET_ADDRESS) + v[0];
+			pc += 2;
             printf("JP V0,%d\n", opcode & GET_ADDRESS);
 			break;
 
 		// Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
 		case 0xC000:
 			v[opcode & GET_X >> 8] = (rand() % 256) & (opcode & GET_8BIT_CONSTANT);
-			this->pc += 2;
+			pc += 2;
             printf("RND V%d,%d\n", (opcode & GET_X) >> 8, opcode & GET_8BIT_CONSTANT);
 			break;
 
@@ -294,22 +295,23 @@ void chip8::nextCycle(){
 		case 0xD000:
             xaux = (opcode & GET_X) >> 8;
             yaux = (opcode & GET_Y) >> 4;
-            v[0xF] = 0;         // Resets the VF
+            height = opcode & GET_4BIT_CONSTANT;
+            v[15] = 0;         // Resets the VF
 
-            for(int y = 0; y < yaux; y++){              // Passes through all lines of the sprite
-                pixel = this->memory[this->I + y];      // Fetches the pixel value on the memory
+            for(int y = 0; y < height; y++){              // Passes through all lines of the sprite
+                pixel = memory[I + y];      // Fetches the pixel value on the memory
                 for(int x = 0; x < 8; x++){             // Passes through all 8 columns of the sprite, in each line
                     if(pixel & (0x80 >> x) != 0){
-                        if(this->display[xaux + x + (yaux + y)*64] == 1){
-                            v[0xF] = 1;                 // Registers the collision
+                        if(display[xaux + x + (yaux + y)*64] == 1){
+                            v[15] = 1;                 // Registers the collision
                         }
-                        this->display[xaux + x + (yaux + y)*64] ^= 1;
+                        display[xaux + x + (yaux + y)*64] ^= 1;
                     }
                 }
             }
 
-            this->drawFlag = 1;
-			this->pc += 2;
+            drawFlag = 1;
+			pc += 2;
             printf("DRW V%d,V%d,%d\n", (opcode & GET_X) >> 8, (opcode & GET_Y) >> 4, opcode & GET_4BIT_CONSTANT);
 			break;
 
@@ -319,31 +321,30 @@ void chip8::nextCycle(){
 				// Skips the next instruction if the key stored in VX is pressed.
 				case 0x000E:
                     printf("SKP V%d\n", (opcode & GET_X) >> 8);
-					if(this->keyboard[v[(opcode & GET_X) >> 8]] != 0)
-						this->pc += 4;
+					if(keyboard[v[(opcode & GET_X) >> 8]] != 0)
+						pc += 4;
 					else
-						this->pc += 2;
+						pc += 2;
 					break;
 
 				// Skips the next instruction if the key stored in VX isn't pressed.
 				case 0x0001:
                     printf("SKNP V%d\n", (opcode & GET_X) >> 8);
-					if(this->keyboard[v[(opcode & GET_X) >> 8]] == 0)
-						this->pc += 4;
+					if(keyboard[v[(opcode & GET_X) >> 8]] == 0)
+						pc += 4;
 					else
-						this->pc += 2;
+						pc += 2;
 					break;
 			}
 
 			break;
 
 		case 0xF000:
-            printf("0xF SHIT\n");
             switch(opcode & GET_8BIT_CONSTANT){
 				// Sets VX to the value of the delay timer.
 				case 0x0007:
-					v[(opcode & GET_X) >> 8] = this->delayTimer;
-					this->pc += 2;
+					v[(opcode & GET_X) >> 8] = delayTimer;
+					pc += 2;
                     printf("LD V%d, DT\n", (opcode & GET_X) >> 8);
 					break;
 
@@ -352,60 +353,65 @@ void chip8::nextCycle(){
                     printf("KEYWAIT V%d, K\n", (opcode & GET_X) >> 8);
 					pressedKey = false;
 					for(int i = 0; i < 16; i++){
-						if(this->keyboard[i] != 0){
+						if(keyboard[i] != 0){
 							v[(opcode & GET_X) >> 8] = i;
 							pressedKey = true;
 						}
 						if(!pressedKey){return;}
                     }
-						this->pc += 2;
+						pc += 2;
                         break;
 
 				// Sets the delay timer to the value of v[X]
 				case 0x0015:
                     printf("LD DT, V%d\n", (opcode & GET_X) >> 8);
-					this->delayTimer = v[(opcode & GET_X) >> 8];
-					this->pc += 2;
+					delayTimer = v[(opcode & GET_X) >> 8];
+					pc += 2;
 					break;
 
 				// Sets the sound timer to the value of v[X]
 				case 0x0018:
                     printf("LD ST, V%d\n", (opcode & GET_X) >> 8);
-					this->soundTimer = v[(opcode & GET_X) >> 8];
-					this->pc += 2;
+					soundTimer = v[(opcode & GET_X) >> 8];
+					pc += 2;
 					break;
 
 				// Adds v[X] to I
 				case 0x001E:
                     printf("ADD I, V%d\n", (opcode & GET_X) >> 8);
-					this->I += v[(opcode & GET_X) >> 8];
-					this->pc += 2;
+                    if(I + v[(opcode & GET_X) >> 8] > 0xFFF)
+                        v[15] = 1;
+                    else
+                        v[15] = 0;
+
+                    I += v[(opcode & GET_X) >> 8];
+					pc += 2;
 					break;
 
 				// Sets I to the location of the sprite for the character in VX.
 				case 0x0029:
                     printf("LD F, V%d\n", (opcode & GET_X) >> 8);
-					this->I = v[(opcode & GET_X) >> 8] * 0x5;
-					this->pc += 2;
+					I = v[(opcode & GET_X) >> 8] * 0x5;
+					pc += 2;
 					break;
 
 				// Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
 				// Implementation by TJA
 				case 0x0033:
                     printf("LD B, V%d\n", (opcode & GET_X) >> 8);
-					this->memory[I]     = v[(opcode & 0x0F00) >> 8] / 100;
-					this->memory[I + 1] = (v[(opcode & 0x0F00) >> 8] / 10) % 10;
-					this->memory[I + 2] = (v[(opcode & 0x0F00) >> 8] % 100) % 10;
-					this->pc += 2;
+					memory[I]     = v[(opcode & 0x0F00) >> 8] / 100;
+					memory[I + 1] = (v[(opcode & 0x0F00) >> 8] / 10) % 10;
+					memory[I + 2] = (v[(opcode & 0x0F00) >> 8] % 100) % 10;
+					pc += 2;
 					break;
 
 				//Stores V0 to VX (including VX) in memory starting at address I.
 				case 0x0055:
                     printf("LD [I], V%d\n", (opcode & GET_X) >> 8);
 					for(int i = 0; i < 16; i++){
-						this->memory[I+i] = v[i];
+						memory[I+i] = v[i];
 					}
-					this->pc += 2;
+					pc += 2;
 					break;
 
 				// Fills V0 to VX (including VX) with values from memory starting at address I.
@@ -413,9 +419,9 @@ void chip8::nextCycle(){
                     printf("I = %d\n", I);
                     printf("LD V%d, [I]\n", (opcode & GET_X) >> 8);
 					for(int i = 0; i < 16 ; i++){
-                        v[i] = this->memory[I+i];
+                        v[i] = memory[I+i];
 					}
-					this->pc += 2;
+					pc += 2;
 					break;
 			}
         break;
@@ -426,9 +432,9 @@ void chip8::nextCycle(){
 	}
 
 	// Decrement the timers if they are greater than zero
-	if(this->delayTimer > 0)
-		this->delayTimer--;
+	if(delayTimer > 0)
+		delayTimer--;
 
-	if(this->soundTimer > 0)
-		this->soundTimer--;
+	if(soundTimer > 0)
+		soundTimer--;
 }
